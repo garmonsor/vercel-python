3d04abaac08fac3daa23ed004d9468ff2152d1d9
timtjoe <ttjoe0@gmail.com>
2024-11-02 22:20:48 +0000

    remove duplicate code

--- app.py
+++ app.py
@@ -12,17 +12,3 @@ def get_item(item_id):
 
 if __name__ == "__main__":
     app.run()
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return jsonify({"message": "Hello, World! Garmonsor"})
-
-@app.route("/items/<int:item_id>")
-def get_item(item_id):
-    return jsonify({"item_id": item_id, "message": "This is an item."})
-
-if __name__ == "__main__":
-    app.run()
